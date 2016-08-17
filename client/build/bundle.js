31375267a934b3e8c8ac3d081ea7c4bc2a9b057e
Richard Henry <r.henry1989@gmail.com>
2016-08-17 09:15:03 +0100

    initial commit

---
+++ client/build/bundle.js
@@ -0,0 +1,98 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+	// [::1] is the IPv6 localhost address.
+	window.location.hostname === '[::1]' ||
+	// 127.0.0.1/8 is considered localhost for IPv4.
+	window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
+	
+	// if ('serviceWorker' in navigator &&
+	//     (window.location.protocol === 'https:' || isLocalhost)) {
+	//   navigator.serviceWorker.register('service-worker.js')
+	//   .then(function(registration) {
+	//     // updatefound is fired if service-worker.js changes.
+	//     registration.onupdatefound = function() {
+	//       // updatefound is also fired the very first time the SW is installed,
+	//       // and there's no need to prompt for a reload at that point.
+	//       // So check here to see if the page is already controlled,
+	//       // i.e. whether there's an existing service worker.
+	//       if (navigator.serviceWorker.controller) {
+	//         // The updatefound event implies that registration.installing is set:
+	//         // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+	//         var installingWorker = registration.installing;
+	//
+	//         installingWorker.onstatechange = function() {
+	//           switch (installingWorker.state) {
+	//             case 'installed':
+	//               // At this point, the old content will have been purged and the
+	//               // fresh content will have been added to the cache.
+	//               // It's the perfect time to display a "New content is
+	//               // available; please refresh." message in the page's interface.
+	//               break;
+	//
+	//             case 'redundant':
+	//               throw new Error('The installing ' +
+	//                               'service worker became redundant.');
+	//
+	//             default:
+	//               // Ignore
+	//           }
+	//         };
+	//       }
+	//     };
+	//   }).catch(function(e) {
+	//     console.error('Error during service worker registration:', e);
+	//   });
+	// }
+	
+	console.log('App started');
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
